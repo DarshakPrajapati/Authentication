5d76608c24bc205e7ed3204e84e75a6f702c50e7
darshak prajapati <darshak1997@outlook.com>
2023-01-12 14:30:39 +0530

    DBContext Class Commit

---
+++ Model/Entities/AppUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Entities
+{
+    public class AppUser : IdentityUser
+    {
+        public String FullName { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+
+
+        public AppUser()
+        {
+            FullName = String.Empty;
+        }
+    }
+}
